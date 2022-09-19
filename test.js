fa9ddc258b77f96b6d11975f15a1ad7096d92ba1
Xillal VPS <julien38.b@gmail.com>
2022-09-19 17:36:46 +0200

    splits

--- test.js
+++ test.js
@@ -12,9 +12,8 @@ const puppeteer = require('puppeteer');
 	});
 
 	await page.goto('https://ffn.extranat.fr/webffn/nat_recherche.php?idact=nat&idrch_id=1390507&idopt=prf&idbas=25');
-	// await page.screenshot({ path: 'example.png' });
-	// let bodyHTML = await page.evaluate(() => document.body.innerHTML);
-	let tbody = await page.evaluate(() => {
+
+	let swimmerData = await page.evaluate(() => {
 		let trList = document.querySelectorAll('table#styleNoBorderNoBottom tbody')[1].children
 		let result = {}
 		let epreuve = 'idk';
@@ -23,16 +22,36 @@ const puppeteer = require('puppeteer');
 			let textContent = tr.textContent.trim().replace(/[\n\t]/g, '')
 			let innerHTML = tr.innerHTML.trim().replace(/[\n\t]/g, '')
 			let splited = textContent.split(' - ')
-			if (splited[1] && splited[1].startsWith('Bassin')) {
+			if (splited[1] && splited[1].startsWith('Bassin')) { // Si y'a le mot "Bassin" dans la deuxième partie du split c'est une épreuve
 				epreuve = splited[0]
 				result[epreuve] = []
-			} else if (epreuve !== 'idk' && textContent.includes('pts')) {
-				result[epreuve].push(textContent)
-			}
+			} else if (epreuve !== 'idk' && textContent.includes('pts')) { // Si y'a le mot "pts" dans le texte c'est un résultat
+				let data = {
+					temps: tr.children[0].textContent.trim(),
+					relais: tr.children[1].textContent != '',
+					pts: tr.children[3].textContent.trim().replace(/[\(\)pts\ ]/g, ''),
+					lieu: {
+						pays: tr.children[4].children[1].textContent.trim().replace(/[\(\)]/g, ''),
+						ville: tr.children[4].children[2].textContent.trim(),
+					},
+					date: tr.children[5].textContent.trim(),
+					niveau: tr.children[6].textContent.trim().replace(/[\[\]]/g, ''),
+					club: tr.children[8].textContent.trim(),
+					lienCompetition: tr.children[7].children[0].href
+				}
+
+				// On regarde si il y as les splits
+				if (tr.children[0].children.length) {
+					let tdList = tr.children[0].firstChild.onmouseover.toString().match(/<tr>(.*)<\/tr>/)[1].toString().replace(/\\/g, '')
+					data.splits = tdList
+				}
+
+				result[epreuve].push(data)
+			} // sinon c'est un truc inutile
 		}
 		return result
 	});
-	console.log(tbody);
+	console.log(swimmerData['100 Nage Libre Messieurs']);
 	await browser.close();
 
 
